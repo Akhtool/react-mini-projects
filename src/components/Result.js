fd6ae7ffc861534cf47a5a139ad9c7804cec8ce0
Zelimhan <Zelimhan1997@mail.ru>
2023-12-12 22:27:24 +0300

    feat: добавить разные картинки для результатов

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,10 +1,14 @@
 import React from "react";
 import questions from "../utils/questions";
+import hooray from '../images/confetti.png';
+import ok from '../images/correct.png';
+import wrong from '../images/wrong.png';
+
 
 function Result({ correct }) {
     return (
       <div className="result">
-        <img alt="Ура!" src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+        <img alt="Ура!" src={(correct === 0) ? wrong : ((correct > 0) && (correct < questions.length)) ? ok : hooray} />
         <h2>Вы отгадали {correct} ответа из {questions.length}</h2>
         <a href='/' >
         <button>Попробовать снова</button>
